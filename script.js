f2685dc3d9e66ba816d6921b472092519352d4c7
HafsaAfzal <hafsaafzal380@gmail.com>
2023-09-18 14:45:52 +0500

    converted successfully numbers and alphabets to 4 bits binary number

--- script.js
+++ script.js
@@ -1,10 +1,52 @@
+function alphaToNum(newkey) {
+    let numKey = [];
+    for (let a = 0; a < newkey.length; a++) {
+
+        switch (newkey[a]) {
+            case "A":
+                numKey.push(10);
+                break;
+            case "B":
+                numKey.push(11);
+                break;
+            case "C":
+                numKey.push(12);
+                break;
+            case "D":
+                numKey.push(13);
+                break;
+            case "E":
+                numKey.push(14);
+                break;
+            case "F":
+                numKey.push(15);
+                break;
+            case "G":
+                numKey.push(16);
+                break;
+            case "H":
+                numKey.push(17);
+                break;
+            case "I":
+                numKey.push(18);
+                break;
+            case "J":
+                numKey.push(19);
+                break;
+            default:
+                numKey.push(newkey[a])
+        }
+    }
+    return numKey;
+}
 function divmod(numerator, denominator) {
     let quotient = Math.floor(numerator / denominator);
     let remainder = numerator % denominator;
     // bin += remainder.toString();
     bin.push(remainder);
     if (quotient === 0) {
-        bin=(bin.reverse()).join("")
+        bin = bin.reverse().join("");
+        // console.log(bin)
         binaryNum.push(bin.padStart(4, '0'));
         bin = []; // Reset the bin string for the next digit
         return;
@@ -13,12 +55,22 @@ function divmod(numerator, denominator) {
     }
 }
 
+
 let binaryNum = [];
-let key = "12345678";
-let bin =[]; // Initialize bin outside the loop
-for (let i = 0; i < key.length; i++) {
+let key = "12345678ABCDEFGH";
+let newkey = []
+console.log(newkey)
+for (let k = 0; k < key.length; k++) {
+    newkey.push(key[k]);
+}
+let newkey1 = alphaToNum(newkey);
+console.log(newkey1)
+
+let bin = []; // Initialize bin outside the loop
+for (let i = 0; i < newkey1.length; i++) {
     // console.log(key[i]);
-    divmod(Number(key[i]), 2);
+    divmod(Number(newkey1[i]), 2);
 }
 
-console.log(binaryNum);
+console.log(binaryNum, "total no of bits in array=length of array X each element length ", (binaryNum.length) * (binaryNum[0].length));
+
